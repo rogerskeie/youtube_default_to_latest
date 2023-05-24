73090c7e0a15aa1386068b850cc8305949023eb1
Roger Skeie <rogerskeie@gmail.com>
2023-05-24 20:12:06 +0100

    Some fixes and code cleanup

--- youtube_default_to_latest.js
+++ youtube_default_to_latest.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Fix YouTube annoyance of defaulting to the new "for you" tab instead of "latest".
 // @author       Roger Skeie
-// @match        https://www.youtube.com/@*/videos
+// @match        https://www.youtube.com/@*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @grant        none
 // ==/UserScript==
@@ -13,13 +13,27 @@
     'use strict';
 
     clickLatestTab();
+    videosTabListener();
 })();
 
 function clickLatestTab() {
-    if (document.querySelector('.yt-chip-cloud-chip-renderer[title="Latest"]')) {
-        document.querySelector('.yt-chip-cloud-chip-renderer[title="Latest"]').parentElement.click();
+    const latestTabElement = document.querySelector('.yt-chip-cloud-chip-renderer[title="Latest"]');
+
+    if (latestTabElement) {
+        latestTabElement.parentElement.click();
     } else {
         setTimeout(clickLatestTab, 100);
+    }
+}
+
+function videosTabListener() {
+    const xpath = "//div[contains(concat(' ',normalize-space(@class),' '),' ytd-c4-tabbed-header-renderer ')][text()='Videos']";
+    const videosTabElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+    if (videosTabElement) {
+        videosTabElement.addEventListener('click', clickLatestTab);
+    } else {
+        setTimeout(videosTabListener, 100);
     }
 }
 
